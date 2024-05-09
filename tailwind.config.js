fe6762e672bf601690dc8960c98024c091f1acea
Sarah Eja <ejasarah1@gmail.com>
2024-05-09 01:38:51 +0100

    My tailwind blogpost project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFFFFF",
+    secondary: "#111729"
+      },
+    fontFamily: {
+      body: 'Nunito'
+    }
+    },
+    
+  },
+  plugins: [],
+}
+
